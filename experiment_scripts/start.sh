0ea9c7ec0b176dcb0e36e78fc6e07cf76f956365
Edoardo Tinto <tinto.edoardo@gmail.com>
2025-10-30 14:35:56 +0100

    Addition to start script

--- experiment_scripts/start.sh
+++ experiment_scripts/start.sh
@@ -9,11 +9,11 @@ node_2="192.168.1.113"
 node_3="192.168.1.126"
 
 # Start the low priority task in each node.
-echo "START LP TASKS"
-sshpass -f node_user_password.txt ssh ubuntu@$node_1 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 3600 experiment_scripts/lp_task_aarch64"; echo ""
-sshpass -f node_user_password.txt ssh ubuntu@$node_2 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 3600 experiment_scripts/lp_task_aarch64"; echo ""
-sshpass -f node_user_password.txt ssh ubuntu@$node_3 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 3600 experiment_scripts/lp_task_aarch64"; echo ""
-echo "LP TASKS STARTED"
+# echo "START LP TASKS"
+# sshpass -f node_user_password.txt ssh ubuntu@$node_1 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 3600 experiment_scripts/lp_task_aarch64"; echo ""
+# sshpass -f node_user_password.txt ssh ubuntu@$node_2 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 3600 experiment_scripts/lp_task_aarch64"; echo ""
+# sshpass -f node_user_password.txt ssh ubuntu@$node_3 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 3600 experiment_scripts/lp_task_aarch64"; echo ""
+# echo "LP TASKS STARTED"
 
 # Clear logs.
 echo "CLEAR THE LOG FILES"
@@ -51,4 +51,4 @@ while read line; do
 
   sleep 20s
 
-done < experiment_data/distances.txt
+done < ../experiment_data/distances.txt
