bf7ab8270af64bcb1a2d5c7d09e1cb62fd6435bc
Edoardo Tinto <tinto.edoardo@gmail.com>
2025-10-30 15:33:43 +0100

    Addition

--- experiment_scripts/start.sh
+++ experiment_scripts/start.sh
@@ -23,32 +23,32 @@ sshpass -f node_user_password.txt ssh ubuntu@$node_3 "cd orchestrating-migrating
 echo "LOG FILES CLEARED"
 
 # Experiment 1.
-while read line; do
+while read -u 9 line; do
 
   read state_0 state_1 state_2 request <<< $line
 
   echo "> CONFIGURATION is $state_0 $state_1 $state_2 $request"
 
   echo "  distributed"
-  sshpass -f node_user_password.txt ssh ubuntu@$node_1 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 20 sudo ./experiment_scripts/start_local.sh 0 distributed node_0 \"$state_0\""; echo ""
-  sshpass -f node_user_password.txt ssh ubuntu@$node_2 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 20 sudo ./experiment_scripts/start_local.sh 1 distributed node_1 \"$state_1\""; echo ""
-  sshpass -f node_user_password.txt ssh ubuntu@$node_3 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 20 sudo ./experiment_scripts/start_local.sh 0 distributed node_2 \"$state_2\""; echo ""
+  sshpass -f node_user_password.txt ssh ubuntu@$node_1 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 30 sudo ./experiment_scripts/start_local.sh 0 distributed node_0 \"$state_0\""; echo ""
+  sshpass -f node_user_password.txt ssh ubuntu@$node_2 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 30 sudo ./experiment_scripts/start_local.sh 1 distributed node_1 \"$state_1\""; echo ""
+  sshpass -f node_user_password.txt ssh ubuntu@$node_3 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 30 sudo ./experiment_scripts/start_local.sh 0 distributed node_2 \"$state_2\""; echo ""
 
   sleep 5s
 
   mosquitto_pub -h 192.168.1.210 -t federation/migration -m "$request"
 
-  sleep 20s
+  sleep 30s
 
   echo "  centralized"
-  sshpass -f node_user_password.txt ssh ubuntu@$node_1 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 20 sudo ./experiment_scripts/start_local.sh 0 centralized node_0 \"$state_0\""; echo ""
-  sshpass -f node_user_password.txt ssh ubuntu@$node_2 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 20 sudo ./experiment_scripts/start_local.sh 1 centralized node_1 \"$state_1\""; echo ""
-  sshpass -f node_user_password.txt ssh ubuntu@$node_3 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 20 sudo ./experiment_scripts/start_local.sh 0 centralized node_2 \"$state_2\""; echo ""
+  sshpass -f node_user_password.txt ssh ubuntu@$node_1 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 30 sudo ./experiment_scripts/start_local.sh 0 centralized node_0 \"$state_0\""; echo ""
+  sshpass -f node_user_password.txt ssh ubuntu@$node_2 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 30 sudo ./experiment_scripts/start_local.sh 1 centralized node_1 \"$state_1\""; echo ""
+  sshpass -f node_user_password.txt ssh ubuntu@$node_3 "cd orchestrating-migrating-computations-poc; echo $(cat node_user_password.txt) | sudo -S screen -d -m timeout 30 sudo ./experiment_scripts/start_local.sh 0 centralized node_2 \"$state_2\""; echo ""
 
   sleep 5s
 
   mosquitto_pub -h 192.168.1.210 -t federation/migration -m "$request"
 
-  sleep 20s
+  sleep 30s
 
-done < experiment_data/distances.txt
+done 9< experiment_data/distances.txt
