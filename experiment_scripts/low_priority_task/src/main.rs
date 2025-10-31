128b793d91e36d91f9fc496343edc658302c5a2f
Edoardo Tinto <tinto.edoardo@gmail.com>
2025-10-31 11:22:43 +0100

    Fix

--- experiment_scripts/low_priority_task/src/main.rs
+++ experiment_scripts/low_priority_task/src/main.rs
@@ -7,11 +7,22 @@ fn main ()
     unsafe { libc::sched_setaffinity(pid, core::mem::size_of::<libc::cpu_set_t> (),&mut cpu_set); }
 
     // Fix the priority.
+    // Schedule as SCHED_OTHER.
     let sched_param = libc::sched_param
+    {
+        sched_priority: 0 as libc::c_int,
+    };
+    unsafe
+        {
+            libc::sched_setscheduler(pid, libc::SCHED_OTHER, &sched_param);
+            libc::nice (-10 as libc::c_int);
+        }
+
+    /* let sched_param = libc::sched_param
     {
         sched_priority: 1 as libc::c_int,
     };
-    unsafe { libc::sched_setscheduler (pid, libc::SCHED_FIFO, &sched_param); }
+    unsafe { libc::sched_setscheduler (pid, libc::SCHED_FIFO, &sched_param); } */
 
     // Then loop forever.
     loop
