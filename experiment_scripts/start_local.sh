e9c540d149fea225501954a17787f3aff61d67b3
Edoardo Tinto <tinto.edoardo@gmail.com>
2025-10-31 10:26:15 +0100

    Fix

--- experiment_scripts/start_local.sh
+++ experiment_scripts/start_local.sh
@@ -41,7 +41,7 @@ echo -n "$5 " >> experiment_data/send.txt
 echo -n "$5 " >> experiment_data/receive.txt
 
 # Start the lp_task.
-sudo timeout $experiment_time ./experiment_scripts/lp_task_aarch64
+# sudo timeout $experiment_time ./experiment_scripts/lp_task_aarch64
 
 # Start the experiment.
 cd experiment_folder/
