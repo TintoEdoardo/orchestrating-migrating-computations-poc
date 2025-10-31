7dc0b2b0c56dca4379baaab8069f59bb81d3b541
Edoardo Tinto <tinto.edoardo@gmail.com>
2025-10-31 14:06:33 +0100

    Add speed up tester

---
+++ experiment_scripts/speed_up_tester/build.sh
@@ -0,0 +1,15 @@
+# Build for aarch64 and x86.
+cargo build --release
+cargo build --release --target aarch64-unknown-linux-gnu
+
+# Remove previous build.
+rm -f out/tester_aarch64
+rm -f out/tester_x86
+
+# Copy the new binaries in out.
+cp target/aarch64-unknown-linux-gnu/release/speed_up_tester out/tester_aarch64
+cp target/release/speed_up_tester out/tester_x86
+
+# Copy a request.
+cp ../../requests/0_0_req/module.wasm out/module.wasm
+cp ../../requests/0_0_req/input_small.pgm out/input_small.pgm
---
+++ experiment_scripts/speed_up_tester/src/main.rs
@@ -0,0 +1,81 @@
+use wasmtime_wasi::{DirPerms, FilePerms};
+
+fn main()
+{
+    let mut start      : std::time::Instant;
+    let mut elapsed_vec: Vec<u64> = Vec::new ();
+    for _i in 0..10
+    {
+        // Get the start time.
+        start = std::time::Instant::now ();
+        struct MyState
+        {
+            wasi              : wasmtime_wasi::preview1::WasiP1Ctx,
+        }
+
+        // Create the engine.
+        let engine = wasmtime::Engine::default ();
+
+        // Load the module.
+        let path_to_module = "module.wasm";
+        let module =
+            wasmtime::Module::from_file (&engine, path_to_module)
+                .expect ("Failed to load wasm file. ");
+
+        // Create the Linker.
+        let mut linker: wasmtime::Linker<MyState>  = wasmtime::Linker::new (&engine);
+        wasmtime_wasi::preview1::add_to_linker_sync (&mut linker, |cx| &mut cx.wasi)
+            .expect ("add_to_linker_sync failed. ");
+
+        // Add the should_migrate function mock.
+        linker.func_wrap ("host", "should_migrate", |caller: wasmtime::Caller<'_, MyState>|
+            {
+                0
+            }
+        ).expect ("func_wrap failed. ");
+
+        // Add the restore_memory, which do nothing right now.
+        linker.func_wrap ("host", "restore_memory", move |caller: wasmtime::Caller<'_, MyState>|
+            {
+                // Do nothing.
+            }
+        )
+            .expect ("func_wrap failed. ");
+
+        let pre = linker.instantiate_pre (&module)
+            .expect ("Instantiate failed. ");
+
+        // Create the Store.
+        let host_path = format! ("./{}", ".");
+        let wasi_ctx = wasmtime_wasi::WasiCtxBuilder::new ()
+            //.inherit_stdio ()
+            .inherit_env ()
+            .preopened_dir(host_path, ".", DirPerms::all(), FilePerms::all())
+            .expect("Unable to config directory. ")
+            .build_p1 ();
+
+        let state = MyState
+        {
+            wasi              : wasi_ctx,
+        };
+        let mut store = wasmtime::Store::new (&engine, state);
+
+        // Instantiate the module.
+        let instance = pre.instantiate (&mut store)
+            .expect ("instantiate failed. ");
+
+        // Invoke the start function of the module.
+        let func = instance.get_func (&mut store, "_start")
+            .expect ("Unable to find function _start. ");
+        let mut result = [];
+
+        let _function_result = func.call (&mut store, &[], &mut result);
+
+        // Record time.
+        elapsed_vec.push (start.elapsed().as_millis () as u64);
+    }
+
+    // Print the avg.
+    let avg_exec_time = elapsed_vec.iter().sum::<u64>() as f64 / elapsed_vec.len() as f64;
+    println!("Avg exec time is {} ms", avg_exec_time);
+}
